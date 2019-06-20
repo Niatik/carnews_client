d081514c9014eda1d9a14b86215d979766f77ef0
Nikita <11559709+Niatik@users.noreply.github.com>
2019-06-20 15:37:53 +0600

    Configure authorization in main.js.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,47 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios);
+
+Vue.use(require('@websanova/vue-auth'), {
+  auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+  http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+  registerData: {
+      url: '/api/register',
+      method: 'POST',
+  },
+  loginData: {
+      url: '/api/login',
+      method: 'POST',
+      redirect: '/home',
+      fetchUser: true,
+  },
+  logoutData: {
+      url: '/api/logout',
+      method: 'POST',
+      redirect: '/',
+      makeRequest: false
+  },
+  refreshData: {
+      url: '/api/refresh',
+      method: 'GET',
+      enabled: true,
+      interval: 30,
+  },
+  fetchData: {
+      url: '/api/me',
+      method: 'GET',
+      enabled: true
+  },
+});
 
 new Vue({
   router,
