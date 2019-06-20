4d4bb9711a4532ff6cad41bb41c344ea75fd4ddc
Nikita <11559709+Niatik@users.noreply.github.com>
2019-06-20 17:26:07 +0600

    Add routes for 'login' and 'register'.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Register from './views/Register'
 
 Vue.use(Router)
 
@@ -14,6 +16,22 @@ export default new Router({
       component: Home,
       meta: {
         auth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        auth: false
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        auth: false
       }
     },
     {
