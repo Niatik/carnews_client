f9b58cfc1d411cee8b5def2e39eb2d1f177d2b93
Nikita <11559709+Niatik@users.noreply.github.com>
2019-06-20 16:01:36 +0600

    Change Application layout.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,109 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list>
+        <v-list-tile
+                v-for="item in menuItems"
+                :key="item.title"
+                :to="item.path">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            {{ item.title }}
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile class="logout-button-side" v-if="$auth.check()" @click="logout">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Выйти</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+      <v-toolbar-side-icon @click="sidebar = !sidebar">
+      </v-toolbar-side-icon>
+      <router-link class="app-title" v-if='!$auth.check()' router-link to="/" tag="span" style="cursor: pointer">
+        {{ appTitle }}
+      </router-link>
+      <router-link class="app-title" v-if='$auth.check()' router-link to="/home" tag="span" style="cursor: pointer">
+        {{ appTitle }}
+      </router-link>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+                flat
+                v-for="item in menuItems"
+                :key="item.title"
+                :to="item.path">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+        <v-btn class="logout-button" v-if="$auth.check()" flat @click="logout">
+          <v-icon left>exit_to_app</v-icon>
+          Выйти
+        </v-btn>
+      </v-toolbar-items>
+
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <div v-if="$auth.ready()">
+        <transition name="fade" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </div>
+
+      <div class="is-loading" v-if="!$auth.ready()">
+        Загрузка ...
+      </div>
     </v-content>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+  export default {
+    data () {
+      return {
+        sidebar: false,
+      }
+    },
+    methods: {
+      logout () {
+        this.$store.dispatch('userLogout', {auth: this.$auth})
+      }
+    },
+    computed: {
+      menuItems () {
+        if (this.$auth.check()) {
+          return [
+            { title: 'Главная', path: '/home', icon: 'home' },
+            { title: 'Категории', path: '/categories'},
+            { title: 'Избранное', path: '/favorites'},
+          ]
+        } else {
+          return [
+            { title: 'Зарегистрироваться', path: '/register', icon: 'face' },
+            { title: 'Войти', path: '/login', icon: 'lock_open' }
+          ]
+        }
+      },
+      appTitle () {
+        return this.$store.state.appTitle
+      }
     }
   }
-}
 </script>
+
+<style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s ease-out;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
