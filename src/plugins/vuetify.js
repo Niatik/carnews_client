ee595d2c515c29bdd275329fbc28f21d2b1fa6f9
Nikita <11559709+Niatik@users.noreply.github.com>
2019-06-20 15:34:11 +0600

    Add Theme to Vuetify.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
+  theme: {
+    primary: '#9c27b0',
+    secondary: '#673ab7',
+    accent: '#ff5722',
+    error: '#f44336',
+    warning: '#3f51b5',
+    info: '#2196f3',
+    success: '#4caf50'
+  }
 })
